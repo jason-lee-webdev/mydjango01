a6ccb4425bd419aedf90f1e229160165e5aec8b9
jason-lee-webdev <jason.lee.webdev@gmail.com>
2024-06-18 23:53:49 +0900

    formatted by black

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+
+def root(request):
+    return HttpResponse("Hello VSCode")
+
+
+urlpatterns = [path("admin/", admin.site.urls), path("", root)]
